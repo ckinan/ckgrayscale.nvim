fc5d1f198419f4054556fb4de002da06f874bda4
ckinan <cesar.kina@gmail.com>
2026-05-05 14:58:31 -0500

    pure black bg

--- lua/lush_theme/ckgrayscale.lua
+++ lua/lush_theme/ckgrayscale.lua
@@ -4,14 +4,14 @@ local hsl = lush.hsl
 ---@diagnostic disable: undefined-global
 return lush(function()
   return {
-    Normal { fg = hsl('#ffffff'), bg = hsl('#101010') },
-    Function { fg = hsl('#ffffff'), bg = hsl('#101010') },
+    Normal { fg = hsl('#ffffff'), bg = hsl('#000000') },
+    Function { fg = hsl('#ffffff'), bg = hsl('#000000') },
     Keyword { fg = hsl('#ffffff'), gui = "bold" },
     Identifier { fg = hsl('#ffffff'), gui = "bold" },
-    StatusLine { fg = hsl('#ffffff'), gui = "bold" },
+    StatusLine { fg = hsl('#ffffff'), gui = "bold", bg = hsl('#101010') },
     Directory { fg = hsl('#ffffff') },
-    Special { fg = hsl('#999999'), bg = hsl('#101010') },
-    String { fg = hsl('#999999'), bg = hsl('#101010') },
-    Comment { fg = hsl('#999999'), bg = hsl('#101010') },
+    Special { fg = hsl('#999999'), bg = hsl('#000000') },
+    String { fg = hsl('#999999'), bg = hsl('#000000') },
+    Comment { fg = hsl('#999999'), bg = hsl('#000000') },
   }
 end)
