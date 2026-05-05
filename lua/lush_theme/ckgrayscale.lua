9c499925e44e6fb80af3d45ce8406c15c47fb8b4
ckinan <cesar.kina@gmail.com>
2026-05-05 14:51:19 -0500

    darker bg

--- lua/lush_theme/ckgrayscale.lua
+++ lua/lush_theme/ckgrayscale.lua
@@ -4,14 +4,14 @@ local hsl = lush.hsl
 ---@diagnostic disable: undefined-global
 return lush(function()
   return {
-    Normal { fg = hsl('#ffffff'), bg = hsl('#181818') },
-    Function { fg = hsl('#ffffff'), bg = hsl('#181818') },
+    Normal { fg = hsl('#ffffff'), bg = hsl('#101010') },
+    Function { fg = hsl('#ffffff'), bg = hsl('#101010') },
     Keyword { fg = hsl('#ffffff'), gui = "bold" },
     Identifier { fg = hsl('#ffffff'), gui = "bold" },
     StatusLine { fg = hsl('#ffffff'), gui = "bold" },
     Directory { fg = hsl('#ffffff') },
-    Special { fg = hsl('#999999'), bg = hsl('#181818') },
-    String { fg = hsl('#999999'), bg = hsl('#181818') },
-    Comment { fg = hsl('#999999'), bg = hsl('#181818') },
+    Special { fg = hsl('#999999'), bg = hsl('#101010') },
+    String { fg = hsl('#999999'), bg = hsl('#101010') },
+    Comment { fg = hsl('#999999'), bg = hsl('#101010') },
   }
 end)
